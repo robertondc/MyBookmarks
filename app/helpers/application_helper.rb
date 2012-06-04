96a45d73e2225f65289b61670030164ab7833a18
Roberto Nogueira <robertondc@gmail.com>
2012-06-03 23:08:54 -0300

    active links according specific path

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def is_active(*args)
+    if args.size == 1
+      "active" if params[:action] == args.first
+    elsif args.size == 2
+      "active" if params[:controller] == args.first && params[:action] == args.last
+    end
+  end
 end
--- app/helpers/bookmarks_helper.rb
+++ app/helpers/bookmarks_helper.rb
@@ -1,2 +1,2 @@
-module BookmarksHelper
+module BookmarksHelper  
 end
